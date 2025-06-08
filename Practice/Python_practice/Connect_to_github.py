0d236527ac2285383ebd7d8a27933eec2cefee22
Mehul Bhatt <146497367+mehulkumarbht@users.noreply.github.com>
2025-06-08 13:15:10 -0400

    Delete Practice/Python_practice directory

--- Practice/Python_practice/Connect_to_github.py
+++
@@ -1,7 +0,0 @@
-#Follow this steps to connect to github
-#1: ls ~/.ssh
-#2: cat ~/.ssh/id_25519.pub
-#3: ssh -T git@github.com , respnse says You've successfully authenticated, 
-#4: Make sure you're in your repo folder, i.e.Devops with pwd
-    # i.e.\\wsl$\Ubuntu\home\eembee\devops, if not then cd \\wsl$\Ubuntu\home\eembee\devops
-#5: git status, response says, On branch main, Your branch is upto date with 'origin/main'
--- Practice/Python_practice/Practice.py
+++
@@ -1,7 +0,0 @@
-from loguru import logger
-from datetime import date
-import time
-import psutil
-import requests
-import os
-
