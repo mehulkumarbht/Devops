a65a807bf8a68f17816fb952d89a5b82166da4dc
mehulkumarbht <mehulkumarbht@gmail.com>
2025-06-23 00:42:48 -0400

    Added file for if_else statement practice :tada:

---
+++ python_practice/if_else.py
@@ -0,0 +1,40 @@
+from loguru import logger
+
+
+# 1: Write a program that takes an integer as input and prints whether it is even or odd.
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     logger.info(f"{num} is even")
+# else:
+#     logger.info(f"{num} is odd")
+
+# 2: Ask the user to enter their age. If the age is 18 or above, print “Eligible to vote”, else print “Not eligible”.
+# age = int(input("Enter your age: "))
+# if age > 18:
+#     logger.info(f"Eligible to vote")
+# else:
+#     logger.info(f"Not eligible")
+
+# 3: Given two numbers, use if-else to print the larger number.
+# num1 = 455
+# num2 = 351
+# if num1 > num2:
+#     logger.info(f"{num1}")
+# else:
+#     logger.info(f"{num2}")
+
+# 4: Take a student’s score as input. If the score is 40 or more, print “Pass”, otherwise print “Fail”.
+# score = int(input("Please enter your score: "))
+# if score >= 40:
+#     logger.info(f"Pass")
+# else:
+#     logger.info(f"Fail")
+
+# 5: Ask the user to enter a number and print whether it's positive, negative, or zero.
+# num = float(input("Please enter number: "))
+# if num > 0:
+#     logger.info(f"positive")
+# elif num == 0:
+#     logger.info(f"zero")
+# else:
+#     logger.info(f"negative")
