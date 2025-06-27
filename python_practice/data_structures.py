9573799b3b52d64b3f4d992feaab62d8f656d93a
mehulkumarbht <mehulkumarbht@gmail.com>
2025-06-26 22:59:32 -0400

    Adding file for data structures :books:

--- python_practice/data_structures.py
+++ python_practice/data_structures.py
@@ -68,12 +68,59 @@ from loguru import logger
 # Given: keys = ["name", "age", "city"]
 # values = ["Alice", 28, "New York"]
 # Combine them into a dictionary using a loop.
-keys = ["name", "age", "city"]
-values = ["Alice", 28, "New York"]
+# keys = ["name", "age", "city"]
+# values = ["Alice", 28, "New York"]
 
-my_dict = {}
-for num in range(len(keys)):
-    my_dict[keys[num]] = values[num]
+# my_dict = {}
+# for num in range(len(keys)):
+#     my_dict[keys[num]] = values[num]
 
-for key, value in my_dict.items():
-    logger.info(f"{key}: {value}")
+# for key, value in my_dict.items():
+#     logger.info(f"{key}: {value}")
+
+# 11: Given a list of numbers, write a program that multiplies each element by 2 and stores the result in a new list.
+# numbers = [1, 2, 3, 4, 5]
+# new_list = []
+
+# for num in range(1, 6, 1):
+#     new_list.append(num * 2)
+# logger.info(f"{new_list}")
+
+# 12:Given a tuple t = (5, 10, 15, 20), write a program to find the index of the value 15.
+# t = (5, 10, 15, 20)
+# index_of_15 = t.index(15)
+# logger.info(f"{index_of_15}")
+
+# 13: Set – Find Difference, Given two sets,
+# Print the elements that are in set a but not in set b.
+# a = {1, 2, 3, 4}
+# b = {3, 4, 5, 6}
+# c = a.difference(b)
+# logger.info(f"{c}")
+
+# 14: Given a dictionary of names and scores, Print only the names of people who scored more than 80.
+# scores = {"Amy": 75, "Bob": 90, "Chad": 60, "Dana": 88}
+# for key, value in scores.items():
+#     if value > 80:
+#         logger.info(f"{key}")
+
+# 15: Mix – Combine Lists into Dictionary with zip() Given two lists:Create a dictionary that maps field names to their values.
+# fields = ["id", "name", "age"]
+# data = [101, "Jane", 29]
+
+# # names = {}
+# # for name in range(len(fields)):
+# #     names[fields[name]] = data[name]
+# # logger.info(f"{names}")
+
+# names = zip(fields, data)
+# print(dict(names))
+
+# 16: Given a list numbers = [1, 2, 3, 4, 5, 6], write a program that creates a new list containing only the odd numbers.
+numbers = [1, 2, 3, 4, 5, 6]
+new_list = []
+for num in numbers:
+    if num % 2 != 0:
+        new_list.append(num)
+
+logger.info(f"{new_list}")
