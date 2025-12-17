f03bd45148e9bb57eaa3de49c3949d7ef08174b0
mehulkumarbht <mehulkumarbht@gmail.com>
2025-12-16 23:24:43 -0500

    Adding first commit for Automation file :fire:

---
+++ python_practice/automation.py
@@ -0,0 +1,20 @@
+from loguru import logger
+
+# excercise:1
+# num1 = float(input("Please add first number:"))
+# num2 = float(input("Please add second number:"))
+# name1 = str(input("Please add your first name:"))
+# name2 = str(input("Please add your last name:"))
+# cities = ["Toronto", "Delhi", "London"]
+# Add = num1 + num2
+# multiply = num1 * num2
+# combine_name = name1 + name2
+# logger.info(f"{Add}")
+# logger.info(f"{multiply}")
+# logger.info(f"{combine_name}")
+# logger.info(f"{cities[0]}")
+# logger.info(f"{cities[-1]}")
+
+# excercise: 2
+# name1 = str(input("Please Enter your first name:"))
+# logger.info(f"Hello {name1}! Today you will learn Python Automation")
