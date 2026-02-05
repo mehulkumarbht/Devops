f52b78fdeeaff7d7cf0c263154d04cd8274fa7b5
mehulkumarbht <mehulkumarbht@gmail.com>
2026-02-04 23:29:50 -0500

    Added file for Day-01 :fire:

--- python_practice/14-days-python-practice.py
+++ python_practice/14-days-python-practice.py
@@ -18,6 +18,12 @@ from loguru import logger
 # logger.info(f"{c}")
 
 # 2: Convert a temperature from Celsius to Fahrenheit.
-c = int(input("Please add temperature in Celcius: "))
-f = c * 1.8 + 32
-logger.info(f"Temprature from Celsius to Fahrenheit is: {f}")
+# c = int(input("Please add temperature in Celcius: "))
+# f = c * 1.8 + 32
+# logger.info(f"Temprature from Celsius to Fahrenheit is: {f}")
+
+# 3: Ask for a name and age; print how old the person will be after 10 years.
+name = str(input("Please enter your name: "))
+age = int(input("Please enter your age: "))
+years_old = age + 10
+logger.info(f"{name} will be {years_old} years old after 10 years")
