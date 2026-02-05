657c69296ce4dee9bdadb338b53919e421010968
mehulkumarbht <mehulkumarbht@gmail.com>
2026-02-04 23:25:04 -0500

    Added file for Day-01 :fire:

--- python_practice/14-days-python-practice.py
+++ python_practice/14-days-python-practice.py
@@ -2,17 +2,22 @@ from loguru import logger
 # Day 1: Variables & Input
 
 # 1: Take two numbers from user input and print their sum, difference, product, and division.
-x = int(input("Please provide first number: "))
-y = int(input("Please provide second number: "))
+# x = int(input("Please provide first number: "))
+# y = int(input("Please provide second number: "))
 # Sum
-z = x + y
-logger.info(f"{z}")
+# z = x + y
+# logger.info(f"{z}")
 # Difference
-a = x - y
-logger.info(f"{a}")
+# a = x - y
+# logger.info(f"{a}")
 # product
-b = x * y
-logger.info(f"{b}")
+# b = x * y
+# logger.info(f"{b}")
 # division
-c = x // y
-logger.info(f"{c}")
+# c = x // y
+# logger.info(f"{c}")
+
+# 2: Convert a temperature from Celsius to Fahrenheit.
+c = int(input("Please add temperature in Celcius: "))
+f = c * 1.8 + 32
+logger.info(f"Temprature from Celsius to Fahrenheit is: {f}")
