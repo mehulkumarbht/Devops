da1b434c696784c127814fec6aa1f9b719a9d9f2
mehulkumarbht <mehulkumarbht@gmail.com>
2026-07-30 00:39:42 -0400

    Adding progress

--- python_practice/splitwise_clone/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "splitwise.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "dev-secret"
